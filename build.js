536283e19ecdaffa7ae9d0a3f20f8312c6868f6c
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2022-03-14 20:34:23 -0400

    change build settings

--- build.js
+++ build.js
@@ -1,5 +1,23 @@
 const pug = require("pug")
 const fs = require("fs")
 
+// index.html rendering
 const html = pug.renderFile("./index.pug")
-fs.writeFileSync("index.html", html)
+
+// directory path
+const dir = "./_site"
+
+// create new directory
+try {
+    // first check if directory already exists
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir)
+        fs.writeFileSync(`${dir}/index.html`, html)
+        console.log("Site is created.")
+    } else {
+        fs.writeFileSync(`${dir}/index.html`, html)
+        console.log("Directory already exists but I created the file anyway.")
+    }
+} catch (err) {
+    console.log(err)
+}
